6d910ef036f94f51530fc79bb7ce1f62001a0779
Asfand Yar <asfandyar687@gmail.com>
2024-08-31 19:50:41 +0500

    feat: added footer

---
+++ app/(components)/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Footer = () => {
+  return (
+    <footer className='bg-violet-900 text-white text-lg font-medium text-center py-3'>
+      <p>Copyrights © InnovaTech Academy 2003-24. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
