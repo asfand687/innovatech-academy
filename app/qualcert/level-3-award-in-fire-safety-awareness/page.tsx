42926df9ec8f3c687d2d3b7c2ee8178757dd4c6a
Asfand Yar <asfandyar687@gmail.com>
2024-09-01 19:43:16 +0500

    feat: added course detail page

---
+++ app/qualcert/level-3-award-in-fire-safety-awareness/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function LevelThreeAwardInFireSafetyAwareness () {
+  return (
+    <div>
+      <div className='flex flex-col gap-8'>
+        <h2 className="text-3xl font-bold pt-10">Level 3 Award in Fire Safety Awareness In Pakistan</h2>
+        <h3 className='text-2xl font-semibold'>Qualcert</h3>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
