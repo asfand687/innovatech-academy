2cf294584e185fa3e468b5f435b6f6d7084d0602
Asfand Yar <asfandyar687@gmail.com>
2024-09-01 19:43:55 +0500

    feat: added course data

---
+++ data/coursesData.ts
@@ -0,0 +1,42 @@
+export const coursesInfo = [
+  {
+    id: 1,
+    img: "/fire-safety-01.jpg",
+    label: "Level 3 Award in Fire Safety awareness.",
+  },
+  {
+    id: 2,
+    img: "/environment-for-business-01.jpg",
+    label: "Level 3 Award in Environment for Business.",
+  },
+  {
+    id: 3,
+    img: "/corporate-risk-essentials-01.jpg",
+    label: "Level 3 Award in Corporate Risk Essentials.",
+  },
+  {
+    id: 4,
+    img: "/electrical-technology-01.jpg",
+    label: "Level 3 Award in Electrical Technology.",
+  },
+  {
+    id: 5,
+    img: "/electrical-technology-level-02.jpg",
+    label: "Level 2 Award in Electrical Technology.",
+  },
+  {
+    id: 6,
+    img: "/civil-technology-01.jpg",
+    label: "Level 3 Award in Civil Technology.",
+  },
+  {
+    id: 7,
+    img: "/civil-technology-level-02.jpg",
+    label: "Level 2 Award in Civil Technology.",
+  },
+  {
+    id: 8,
+    img: "/construction-site-investigation-01.jpg",
+    label: "Award in Construction Site Incident Investigation.",
+  },
+]
