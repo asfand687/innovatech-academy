5e55de68c0dab8b3eeabd69f7a31e386a2349f21
Asfand Yar <asfandyar687@gmail.com>
2024-09-02 00:39:27 +0500

    feat: updated navbar

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -25,28 +25,16 @@ const Navbar = () => {
 
           <div className="hidden lg:flex lg:items-center lg:justify-center lg:space-x-10">
             <Link href="#" title="" legacyBehavior>
-              <a className="text-base text-white transition-all duration-200 hover:text-opacity-80">Features</a>
+              <a className="text-base text-white transition-all duration-200 hover:text-opacity-80">Home</a>
             </Link>
             <Link href="#" title="" legacyBehavior>
-              <a className="text-base text-white transition-all duration-200 hover:text-opacity-80">Solutions</a>
+              <a className="text-base text-white transition-all duration-200 hover:text-opacity-80">Courses</a>
             </Link>
-            <Link href="#" title="" legacyBehavior>
-              <a className="text-base text-white transition-all duration-200 hover:text-opacity-80">Resources</a>
+            <Link href="/verification" title="" legacyBehavior>
+              <a className="text-base text-white transition-all duration-200 hover:text-opacity-80">Verification</a>
             </Link>
             <Link href="#" title="" legacyBehavior>
               <a className="text-base text-white transition-all duration-200 hover:text-opacity-80">Pricing</a>
-            </Link>
-          </div>
-
-          <div className="lg:flex lg:items-center lg:justify-end lg:space-x-6 sm:ml-auto">
-            <Link href="#" title="" legacyBehavior>
-              <a className="hidden text-base text-white transition-all duration-200 lg:inline-flex hover:text-opacity-80">Log in</a>
-            </Link>
-
-            <Link href="#" title="" legacyBehavior>
-              <a className="inline-flex items-center justify-center px-3 sm:px-5 py-2.5 text-sm sm:text-base font-semibold transition-all duration-200 text-white bg-white/20 hover:bg-white/40 focus:bg-white/40 rounded-lg" role="button">
-                Apply for free
-              </a>
             </Link>
           </div>
 
